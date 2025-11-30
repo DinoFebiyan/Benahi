5acae98cad1f48eb93410fe752f2669819787643
DianRestu <DianRestu13>
2025-11-30 11:29:04 +0700

    push ulang

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,8 +23,9 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            // AdminSeeder::class,
+            //AdminSeeder::class,
             TeknisiSeeder::class,
+            TeknisiUpdateSeeder::class
         ]);
     }
 }
